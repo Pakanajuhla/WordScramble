13aa60db67b16f6f15d3215303f0665bdba37305
Vladyslav <135268924+Pakanajuhla@users.noreply.github.com>
2023-11-17 16:37:54 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Владислав on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
